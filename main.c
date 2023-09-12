49631c0e3e90c1eccc748ffce044eeccfd36b36d
arghyawning <arghya.roy@research.iiit.ac.in>
2023-09-12 20:50:59 +0530

    corrected redirection, updated gitignore

--- main.c
+++ main.c
@@ -46,7 +46,7 @@ int main()
 
     signal(SIGINT, ctrlc);
     // signal(SIGTSTP, ctrlz);
-    printf("coming\n");
+    // printf("coming\n");
 
     struct sigaction new_action, old_action;
 
@@ -88,194 +88,11 @@ int main()
             if (input == NULL || strlen(input) == 0)
                 continue;
 
-            // checking for i/o redirection
-            int iored = 0; /*
-             0 for no redirection,
-             1 for input redirection,
-             2 for output redirection (w),
-             3 for output redirection (a),
-             4 for both input and output redirection (w),
-             5 for both input and output redirection (a)*/
-            char inputcopy[10000];
-            strcpy(inputcopy, input);
-            if (strstr(inputcopy, "<<") != NULL)
-            {
-                printf(ERROR_COLOR "Error: Invalid redirection type\n" DEFAULT_COLOR);
-                continue;
-            }
-            char iredf[4096];
-            char oredf[4096];
-            strcpy(iredf, "");
-            strcpy(oredf, "");
-            char *iort;
-            char *redtemp = strstr(inputcopy, ">>");
-            if (redtemp == NULL)
-            {
-                // printf("redtemp is null\n");
-                redtemp = strstr(inputcopy, ">");
-                if (redtemp == NULL)
-                {
-                    // printf("redtemp is null\n");
-                    redtemp = strstr(inputcopy, "<");
-                    if (redtemp != NULL)
-                    {
-                        iored = 1;
-                        strcpy(iredf, redtemp + 1);
-                        trimstr(iredf);
-                    }
-                }
-                else
-                {
-                    iored = 2;
-
-                    // before and after >
-                    char before[4096];
-                    char after[4096];
-                    char *bef = strtok_r(inputcopy, ">", &iort);
-                    strcpy(before, bef);
-                    strcpy(after, redtemp + 1);
-                    trimstr(after);
-                    trimstr(before);
-                    // printf("%s, %s\n", before, after);
-
-                    char tt[4096];
-                    strcpy(tt, after);
-                    iort = NULL;
-                    bef = strtok_r(tt, "<", &iort);
-                    strcpy(oredf, bef);
-
-                    // checking for input redirection before and after >
-                    redtemp = strstr(before, "<");
-                    if (redtemp == NULL)
-                    {
-                        // printf("redtemp is null\n");
-                        redtemp = strstr(after, "<");
-                        if (redtemp != NULL)
-                        {
-                            iored = 4;
-                            strcpy(iredf, redtemp + 1);
-                            trimstr(iredf);
-                        }
-                    }
-                    else
-                    {
-                        iored = 4;
-                        strcpy(iredf, redtemp + 1);
-                        trimstr(iredf);
-                    }
-                }
-            }
-            else
-            {
-                iored = 3;
-
-                // before and after >>
-                char before[4096];
-                char after[4096];
-                char *bef = strtok_r(inputcopy, ">", &iort);
-                strcpy(before, bef);
-                strcpy(after, redtemp + 2);
-                trimstr(after);
-                trimstr(before);
-                // printf("%s, %s\n", before, after);
-
-                char tt[4096];
-                strcpy(tt, after);
-                iort = NULL;
-                bef = strtok_r(tt, "<", &iort);
-                strcpy(oredf, bef);
-
-                // checking for input redirection before and after >>
-                redtemp = strstr(before, "<");
-                if (redtemp == NULL)
-                {
-                    // printf("redtemp is null\n");
-                    redtemp = strstr(after, "<");
-                    if (redtemp != NULL)
-                    {
-                        iored = 5;
-                        strcpy(iredf, redtemp + 1);
-                        trimstr(iredf);
-                    }
-                }
-                else
-                {
-                    iored = 5;
-                    strcpy(iredf, redtemp + 1);
-                    trimstr(iredf);
-                }
-            }
-            // printf("iored=%d\n", iored);
-            // printf("iredf=%s\n", iredf);
-            // printf("oredf=%s\n", oredf);
-
-            // redirection
-            switch (iored)
-            {
-            case 0:
-                break;
-            case 1:
-                // printf("input redirection only\n");
-                FILE *iredir = freopen(iredf, "r", stdin);
-                if (iredir == NULL)
-                {
-                    printf(ERROR_COLOR "Error: No such file or directory\n" DEFAULT_COLOR);
-                    continue;
-                }
-                break;
-            case 2:
-                FILE *oredirw = freopen(oredf, "w", stdout);
-                if (oredirw == NULL)
-                {
-                    printf(ERROR_COLOR "Error: No such file or directory\n" DEFAULT_COLOR);
-                    continue;
-                }
-                break;
-            case 3:
-                FILE *oredira = freopen(oredf, "a", stdout);
-                if (oredira == NULL)
-                {
-                    printf(ERROR_COLOR "Error: No such file or directory\n" DEFAULT_COLOR);
-                    continue;
-                }
-                break;
-            case 4:
-                printf("input and output redirection (w)\n");
-                FILE *ioredir4 = freopen(iredf, "r", stdin);
-                if (ioredir4 == NULL)
-                {
-                    printf(ERROR_COLOR "Error: No such file or directory\n" DEFAULT_COLOR);
-                    continue;
-                }
-                FILE *ioredirw = freopen(oredf, "w", stdout);
-                if (ioredirw == NULL)
-                {
-                    printf(ERROR_COLOR "Error: No such file or directory\n" DEFAULT_COLOR);
-                    continue;
-                }
-                break;
-            case 5:
-                printf("input and output redirection (a)\n");
-                FILE *ioredir5 = freopen(iredf, "r", stdin);
-                if (ioredir5 == NULL)
-                {
-                    printf(ERROR_COLOR "Error: No such file or directory\n" DEFAULT_COLOR);
-                    continue;
-                }
-                FILE *ioredira = freopen(oredf, "a", stdout);
-                if (ioredira == NULL)
-                {
-                    printf(ERROR_COLOR "Error: No such file or directory\n" DEFAULT_COLOR);
-                    continue;
-                }
-                break;
-            }
-
             // tokenising the input
             char temp[strlen(input) + 1];
             strcpy(temp, input);
-            char *inpworedir = strtok(temp, "<>");
-            strcpy(temp, inpworedir);
+            // char *inpworedir = strtok(temp, "<>");
+            // strcpy(temp, inpworedir);
             // printf("inp w/o redir=%s\n", temp);
 
             char *x;
@@ -294,6 +111,193 @@ int main()
                     fflush(stdout);
                     exit(0);
                 }
+
+                // checking for i/o redirection
+                int iored = 0; /*
+                 0 for no redirection,
+                 1 for input redirection,
+                 2 for output redirection (w),
+                 3 for output redirection (a),
+                 4 for both input and output redirection (w),
+                 5 for both input and output redirection (a)*/
+                char inputcopy[10000];
+                strcpy(inputcopy, command);
+                if (strstr(inputcopy, "<<") != NULL)
+                {
+                    printf(ERROR_COLOR "Error: Invalid redirection type\n" DEFAULT_COLOR);
+                    continue;
+                }
+                char iredf[4096];
+                char oredf[4096];
+                strcpy(iredf, "");
+                strcpy(oredf, "");
+                char *iort;
+                char *redtemp = strstr(inputcopy, ">>");
+                if (redtemp == NULL)
+                {
+                    // printf("%s is null\n", inputcopy);
+                    redtemp = strstr(inputcopy, ">");
+                    if (redtemp == NULL)
+                    {
+                        // printf("redtemp is null\n");
+                        redtemp = strstr(inputcopy, "<");
+                        if (redtemp != NULL)
+                        {
+                            iored = 1;
+                            strcpy(iredf, redtemp + 1);
+                            trimstr(iredf);
+                        }
+                    }
+                    else
+                    {
+                        iored = 2;
+
+                        // before and after >
+                        char before[4096];
+                        char after[4096];
+                        char *bef = strtok_r(inputcopy, ">", &iort);
+                        strcpy(before, bef);
+                        strcpy(after, redtemp + 1);
+                        trimstr(after);
+                        trimstr(before);
+                        // printf("%s, %s\n", before, after);
+
+                        char tt[4096];
+                        strcpy(tt, after);
+                        iort = NULL;
+                        bef = strtok_r(tt, "<", &iort);
+                        strcpy(oredf, bef);
+
+                        // checking for input redirection before and after >
+                        redtemp = strstr(before, "<");
+                        if (redtemp == NULL)
+                        {
+                            // printf("redtemp is null\n");
+                            redtemp = strstr(after, "<");
+                            if (redtemp != NULL)
+                            {
+                                iored = 4;
+                                strcpy(iredf, redtemp + 1);
+                                trimstr(iredf);
+                            }
+                        }
+                        else
+                        {
+                            iored = 4;
+                            strcpy(iredf, redtemp + 1);
+                            trimstr(iredf);
+                        }
+                    }
+                }
+                else
+                {
+                    iored = 3;
+
+                    // before and after >>
+                    char before[4096];
+                    char after[4096];
+                    char *bef = strtok_r(inputcopy, ">", &iort);
+                    strcpy(before, bef);
+                    strcpy(after, redtemp + 2);
+                    trimstr(after);
+                    trimstr(before);
+                    // printf("%s, %s\n", before, after);
+
+                    char tt[4096];
+                    strcpy(tt, after);
+                    iort = NULL;
+                    bef = strtok_r(tt, "<", &iort);
+                    strcpy(oredf, bef);
+
+                    // checking for input redirection before and after >>
+                    redtemp = strstr(before, "<");
+                    if (redtemp == NULL)
+                    {
+                        // printf("redtemp is null\n");
+                        redtemp = strstr(after, "<");
+                        if (redtemp != NULL)
+                        {
+                            iored = 5;
+                            strcpy(iredf, redtemp + 1);
+                            trimstr(iredf);
+                        }
+                    }
+                    else
+                    {
+                        iored = 5;
+                        strcpy(iredf, redtemp + 1);
+                        trimstr(iredf);
+                    }
+                }
+                // printf("iored=%d\n", iored);
+                // printf("iredf=%s\n", iredf);
+                // printf("oredf=%s\n", oredf);
+
+                // redirection
+                switch (iored)
+                {
+                case 0:
+                    break;
+                case 1:
+                    // printf("input redirection only\n");
+                    FILE *iredir = freopen(iredf, "r", stdin);
+                    if (iredir == NULL)
+                    {
+                        printf(ERROR_COLOR "Error: No such file or directory\n" DEFAULT_COLOR);
+                        continue;
+                    }
+                    break;
+                case 2:
+                    FILE *oredirw = freopen(oredf, "w", stdout);
+                    if (oredirw == NULL)
+                    {
+                        printf(ERROR_COLOR "Error: No such file or directory\n" DEFAULT_COLOR);
+                        continue;
+                    }
+                    break;
+                case 3:
+                    FILE *oredira = freopen(oredf, "a", stdout);
+                    if (oredira == NULL)
+                    {
+                        printf(ERROR_COLOR "Error: No such file or directory\n" DEFAULT_COLOR);
+                        continue;
+                    }
+                    break;
+                case 4:
+                    // printf("input and output redirection (w)\n");
+                    FILE *ioredir4 = freopen(iredf, "r", stdin);
+                    if (ioredir4 == NULL)
+                    {
+                        printf(ERROR_COLOR "Error: No such file or directory\n" DEFAULT_COLOR);
+                        continue;
+                    }
+                    FILE *ioredirw = freopen(oredf, "w", stdout);
+                    if (ioredirw == NULL)
+                    {
+                        printf(ERROR_COLOR "Error: No such file or directory\n" DEFAULT_COLOR);
+                        continue;
+                    }
+                    break;
+                case 5:
+                    // printf("input and output redirection (a)\n");
+                    FILE *ioredir5 = freopen(iredf, "r", stdin);
+                    if (ioredir5 == NULL)
+                    {
+                        printf(ERROR_COLOR "Error: No such file or directory\n" DEFAULT_COLOR);
+                        continue;
+                    }
+                    FILE *ioredira = freopen(oredf, "a", stdout);
+                    if (ioredira == NULL)
+                    {
+                        printf(ERROR_COLOR "Error: No such file or directory\n" DEFAULT_COLOR);
+                        continue;
+                    }
+                    break;
+                }
+
+                char *inpworedir = strtok(temp, "<>");
+                strcpy(command, inpworedir);
+
                 char cmtemp[strlen(command) + 1];
                 strcpy(cmtemp, command);
 
@@ -385,32 +389,30 @@ int main()
                         }
                     }
                 }
-
+                // resetting the redirection
+                switch (iored)
+                {
+                case 0:
+                    break;
+                case 1:
+                    freopen("/dev/tty", "r", stdin);
+                    break;
+                case 2:
+                    freopen("/dev/tty", "w", stdout);
+                    break;
+                case 3:
+                    freopen("/dev/tty", "w", stdout);
+                    break;
+                case 4:
+                    freopen("/dev/tty", "r", stdin);
+                    freopen("/dev/tty", "w", stdout);
+                    break;
+                case 5:
+                    freopen("/dev/tty", "r", stdin);
+                    freopen("/dev/tty", "w", stdout);
+                    break;
+                }
                 command = strtok_r(NULL, ";", &x);
-            }
-
-            // resetting the redirection
-            switch (iored)
-            {
-            case 0:
-                break;
-            case 1:
-                freopen("/dev/tty", "r", stdin);
-                break;
-            case 2:
-                freopen("/dev/tty", "w", stdout);
-                break;
-            case 3:
-                freopen("/dev/tty", "w", stdout);
-                break;
-            case 4:
-                freopen("/dev/tty", "r", stdin);
-                freopen("/dev/tty", "w", stdout);
-                break;
-            case 5:
-                freopen("/dev/tty", "r", stdin);
-                freopen("/dev/tty", "w", stdout);
-                break;
             }
         }
     }
